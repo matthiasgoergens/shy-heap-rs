fb49148f0f58070b0808c77cf2c6182daf20c34b
Matthias Goergens <matthias.goergens@gmail.com>
2025-03-17 12:22:27 +0800

    Flat merge pairs

--- src/pairing.rs
+++ src/pairing.rs
@@ -1,9 +1,11 @@
 // Soft heaps based on pairing heaps.
 // We do min-heaps by default.
 
+use std::{collections::VecDeque, vec};
+
 use itertools::Itertools;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct Pool<T> {
     pub item: T,
     pub count: isize,
@@ -37,7 +39,7 @@ impl<T> Pool<T> {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct Pairing<T> {
     pub key: Pool<T>,
     pub children: Vec<Pairing<T>>,
@@ -105,22 +107,62 @@ impl<T: Ord> Pairing<T> {
     }
 
     pub fn merge_pairs(items: Vec<Self>) -> Option<Self> {
+        // We can probably also just chunk M items at a time (via a reduce),
+        // sift every full chunk (or every but the last chunk?) and then reduce everything once (without any sifting).
+        // Instead of our tree based approach.
         let mut items = items;
         loop {
-            for _ in 0..EVERY - 1 {
-                if items.len() < 2 {
-                    return items.into_iter().reduce(Self::meld);
-                }
+            for _ in 0..EVERY {
                 items = items
                     .into_iter()
                     .chunks(2)
                     .into_iter()
                     .filter_map(|pair| pair.reduce(Self::meld))
-                    .collect()
+                    .collect();
+                if items.len() < 2 {
+                    return items.into_iter().reduce(Self::meld);
+                }
             }
+            // This is potentially arbitrarily recursive.
             items = items.into_iter().map(Self::sift_min).collect();
         }
     }
+
+    pub fn merge_pairs_flat(items: Vec<Self>) -> Option<Self> {
+        items
+            .into_iter()
+            .chunks(CHUNKS)
+            .into_iter()
+            .filter_map(|chunk| chunk.reduce(Self::meld))
+            .reduce(|a, b| a.meld(b).sift_min())
+    }
+
+    pub fn check_heap_property(&self) -> bool {
+        let Pairing { key, children } = self;
+        children.iter().all(|child| key.item <= child.key.item)
+            && children.iter().all(Self::check_heap_property)
+    }
 }
 
+// Get all non-corrupted elements still in the heap.
+impl<T> From<Pairing<T>> for Vec<T> {
+    fn from(pairing: Pairing<T>) -> Self {
+        // Pre-order traversal.
+        let mut items = vec![];
+        let mut todo = VecDeque::from([pairing]);
+        while let Some(pairing) = todo.pop_front() {
+            let Pairing {
+                key: Pool { item, count },
+                children,
+            } = pairing;
+            assert!(count >= 0);
+            todo.extend(children);
+            items.push(item);
+        }
+        items
+    }
+}
+
+/// This one controls the soft heap's 'epsilon' corruption behaviour.
 const EVERY: usize = 3;
+const CHUNKS: usize = 4;
