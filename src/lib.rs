8ed80bfcbeb07581d3ad99e24d0c67f6f459870e
Matthias Goergens <matthias.goergens@gmail.com>
2025-03-18 10:17:22 +0800

    Stuff

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,19 @@ pub mod pairing;
 pub mod schubert;
 pub mod trees;
 
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn string_sum(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
 }
